fe4d8064c98166e3b3011c8a72447a559271c622
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2022-09-02 18:31:29 -0500

    Added random relation generation

--- hashtable_gpu.cu
+++ hashtable_gpu.cu
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <iostream>
 #include <assert.h>
+#include <cstdlib>
 #include <thrust/count.h>
 #include "utils.h"
 
@@ -16,6 +17,23 @@ inline cudaError_t checkCuda(cudaError_t result) {
 }
 
 
+void generate_random_relation(int *relation, int relation_rows, int relation_columns, double max_duplicate_percentage) {
+    double temp = (ceil)((1 - (max_duplicate_percentage / 100)) * relation_rows);
+
+    int max_number = temp;
+    for (int i = 0; i < relation_rows; i++) {
+        int key = (i % max_number) + 1;
+        relation[(i * relation_columns) + 0] = key;
+        for (int j = 1; j < relation_columns; j++) {
+            relation[(i * relation_columns) + j] = (rand() % 500) + 1;
+        }
+    }
+    double duplicate = ((double) (relation_rows - max_number) / relation_rows) * 100;
+    cout << "Unique max number " << max_number << ", out of " << relation_rows << endl;
+    cout << fixed << "Duplicate percentage: " << duplicate << endl;
+}
+
+
 struct Entity {
     int key;
     int value;
@@ -23,7 +41,9 @@ struct Entity {
 
 struct is_match {
     int key;
+
     is_match(int searched_key) : key(searched_key) {};
+
     __host__ __device__
     bool operator()(Entity &x) {
         return x.key == key;
@@ -44,7 +64,7 @@ void build_hash_table(Entity *hash_table, int hash_table_row_size,
             hash_table[position].value = value;
             break;
         }
-        position = (position + 1) % hash_table_row_size;
+        position = (position + 1) & (hash_table_row_size - 1);
     }
 }
 
@@ -59,7 +79,7 @@ void get_match_count(Entity *hash_table, int hash_table_row_size, int *match_cou
         } else if (hash_table[position].key == 0) {
             return;
         }
-        position = (position + 1) % hash_table_row_size;
+        position = (position + 1) & (hash_table_row_size - 1);
     }
 }
 
@@ -77,10 +97,19 @@ void search_hash_table(Entity *hash_table, int hash_table_row_size,
         } else if (hash_table[position].key == 0) {
             return;
         }
-        position = (position + 1) % hash_table_row_size;
+        position = (position + 1) & (hash_table_row_size - 1);
     }
 }
 
+
+void show_rate(int n, string message,
+               chrono::high_resolution_clock::time_point time_point_begin,
+               chrono::high_resolution_clock::time_point time_point_end) {
+    chrono::duration<double> time_span = time_point_end - time_point_begin;
+    double total_time = time_span.count();
+    int rate = n / total_time;
+    cout << fixed << message << ": " << rate << " keys/second" << endl;
+}
 
 void show_hash_table(Entity *hash_table, int hash_table_row_size, const char *hash_table_name) {
     int count = 0;
@@ -98,7 +127,8 @@ void show_hash_table(Entity *hash_table, int hash_table_row_size, const char *ha
 
 
 void gpu_hash_table(const char *data_path, char separator,
-                    int relation_rows, int relation_columns) {
+                    int relation_rows, int relation_columns, double load_factor, int key,
+                    int max_duplicate_percentage) {
     std::chrono::high_resolution_clock::time_point time_point_begin;
     std::chrono::high_resolution_clock::time_point time_point_end;
     std::chrono::high_resolution_clock::time_point time_point_begin_outer;
@@ -112,8 +142,9 @@ void gpu_hash_table(const char *data_path, char separator,
     int *relation;
     int *search_result;
     Entity *hash_table;
-    double load_factor = 0.45;
+
     int hash_table_row_size = (int) relation_rows / load_factor;
+    hash_table_row_size = pow(2, ceil(log(hash_table_row_size) / log(2)));
 
     size_t relation_size = relation_rows * relation_columns * sizeof(int);
     size_t hash_table_size = hash_table_row_size * sizeof(Entity);
@@ -124,10 +155,15 @@ void gpu_hash_table(const char *data_path, char separator,
     checkCuda(cudaMallocManaged(&relation, relation_size));
     checkCuda(cudaMallocManaged(&hash_table, hash_table_size));
     checkCuda(cudaMemPrefetchAsync(relation, relation_size, deviceId));
+    const char *empty = "";
+    if (strcmp(data_path, empty) == 0) {
+        generate_random_relation(relation, relation_rows, relation_columns, max_duplicate_percentage);
+    } else {
+        get_relation_from_file_gpu(relation, data_path,
+                                   relation_rows, relation_columns,
+                                   separator);
+    }
 
-    get_relation_from_file_gpu(relation, data_path,
-                               relation_rows, relation_columns,
-                               separator);
 //    show_relation(relation, relation_rows, relation_columns, "Relation 1", -1, 1);
     time_point_end = chrono::high_resolution_clock::now();
     show_time_spent("Read relation", time_point_begin, time_point_end);
@@ -149,9 +185,9 @@ void gpu_hash_table(const char *data_path, char separator,
 //    show_hash_table(hash_table, hash_table_row_size, "Hash table");
     time_point_end = chrono::high_resolution_clock::now();
     show_time_spent("Hash table build", time_point_begin, time_point_end);
+    show_rate(relation_rows, "Hash table build", time_point_begin, time_point_end);
 
     time_point_begin = chrono::high_resolution_clock::now();
-    int key = 539;
 
 //    block_size = 1;
 //    grid_size = 1;
@@ -198,10 +234,15 @@ int main() {
     char separator = '\t';
     int relation_rows, relation_columns;
     relation_columns = 2;
-    relation_rows = 10;
+    relation_rows = 13;
+    double load_factor = 0.3;
 //    data_path = "data/data_4.txt";
     data_path = "data/link.facts_412148.txt";
+//    data_path = "";
+    int key = 3;
+    int max_duplicate_percentage = 30;
+
     gpu_hash_table(data_path, separator,
-                   relation_rows, relation_columns);
+                   relation_rows, relation_columns, load_factor, key, max_duplicate_percentage);
     return 0;
 }
