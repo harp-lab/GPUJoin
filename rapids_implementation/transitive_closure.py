de1fd515d27e3116f3df6d67d6f8f4153efe6cd9
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2022-09-25 18:44:05 -0500

    added stable sort before unique api call in tc

--- rapids_implementation/transitive_closure.py
+++ rapids_implementation/transitive_closure.py
@@ -60,7 +60,7 @@ def get_transitive_closure(dataset):
         relation_2.columns = COLUMN_NAMES[::-1]
         i += 1
         del temp_projection
-        print(f"i: {i}, projection size: {x}, rows: {current_result_size}")
+        # print(f"i: {i}, projection size: {x}, rows: {current_result_size}")
     end_time_outer = time.perf_counter()
     time_took = end_time_outer - start_time_outer
     time_took = f"{time_took:.6f}"
@@ -111,14 +111,14 @@ def generate_benchmark(iterative=True, datasets=None):
 
 if __name__ == "__main__":
     generate_benchmark(iterative=False, datasets={
-        # "cal.cedge": "../data/data_21693.txt",
-        # "SF.cedge": "../data/data_223001.txt",
+        "cal.cedge": "../data/data_21693.txt",
+        "SF.cedge": "../data/data_223001.txt",
         "TG.cedge": "../data/data_23874.txt",
-        # "OL.cedge": "../data/data_7035.txt",
+        "OL.cedge": "../data/data_7035.txt",
         # "data_4": "../data/data_4.txt",
         # "data_22": "../data/data_22.txt",
-        # "p2p-Gnutella09": "../data/data_26013.txt",
-        # "p2p-Gnutella04": "../data/data_39994.txt"
+        "p2p-Gnutella09": "../data/data_26013.txt",
+        "p2p-Gnutella04": "../data/data_39994.txt"
     })
 
     generate_benchmark(iterative=False, datasets={
