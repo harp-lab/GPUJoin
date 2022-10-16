1992cc7d58b6f67b253cee586504d4c61a72afe0
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2022-10-16 14:30:27 -0500

    added dataset generation script for cyclic and acyclic graph

--- rapids_implementation/dataset_generation.py
+++ rapids_implementation/dataset_generation.py
@@ -4,10 +4,10 @@ import cudf
 
 def generate_csv(filename, number_of_rows,
                  minimum_number=1,
-                 maximum_number=500, string_graph=True):
+                 maximum_number=500, acyclic_graph=False, cyclic_graph=False):
     ar = []
     count = 0
-    if string_graph:
+    if acyclic_graph:
         a = 1
         b = 2
         while count < number_of_rows:
@@ -15,6 +15,15 @@ def generate_csv(filename, number_of_rows,
             a += 1
             b += 1
             count += 1
+    elif cyclic_graph:
+        a = 1
+        b = 2
+        while count < number_of_rows - 1:
+            ar.append((a, b))
+            a += 1
+            b += 1
+            count += 1
+        ar.append((a, 1))
     else:
         while count < number_of_rows:
             a = random.randint(minimum_number, maximum_number)
@@ -40,9 +49,15 @@ def generate_datasets():
 
 
 if __name__ == "__main__":
+    # for i in range(990, 50000, 1000):
+    #     filename = f"../data/data_{i}.txt"
+    #     generate_csv(filename, i, acyclic_graph=True)
+    i = (10**7) + 2
+    filename = f"../data/data_{i}.txt"
+    generate_csv(filename, i, acyclic_graph=True)
 
-    for i in range(990, 50000, 1000):
-        filename = f"../data/data_{i}.txt"
-        generate_csv(filename, i, string_graph=True)
+    i = (10**7) + 1
+    filename = f"../data/data_{i}.txt"
+    generate_csv(filename, i, cyclic_graph=True)
 
     # generate_datasets()
